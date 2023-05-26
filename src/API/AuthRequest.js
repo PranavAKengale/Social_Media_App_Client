13b438b9edeab457ce57215a15693bf8c5cf2c1b
Pranav Arun Kengale <102127066+PranavAKengale@users.noreply.github.com>
2023-05-26 08:09:38 -0400

    Update AuthRequest.js

--- src/API/AuthRequest.js
+++ src/API/AuthRequest.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const API = axios.create({ baseURL: 'https://social-media-app-server-flax.vercel.app' });
+const API = axios.create({ baseURL: 'https://social-media-ncjn.onrender.com' });
 
 export const logIn = (formData)=> { return API.post('/auth/login',formData)}
 export const signUp = (formData) =>{return API.post('/auth/register', formData)}
